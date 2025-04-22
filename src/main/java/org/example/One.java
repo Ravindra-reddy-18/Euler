6199a00c93d06b5af1aabe91042b2ae31b782485
ravindrareddyy88 <ravindrareddyy88@gmail.com>
2025-04-22 11:59:52 -0500

    Problem number one

--- src/main/java/org/example/One.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class One {
-    public static void main(String[] args){
-        System.out.println("Hello dude");
-    }
-}
---
+++ src/main/java/org/example/ProblemOne.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class ProblemOne {
+
+    //method to find sum of multiples of 3 or 5 below a specified number x
+    public static int sumOfMultiples(int x){
+        int sum=0;
+        for(int i=3;i<x;i++){
+            if(i%3==0||i%5==0)
+                sum+=i;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args){
+        System.out.println("The sum of multiples of 3 or 5 below 1000 is = "+sumOfMultiples(1000));
+    }
+}
