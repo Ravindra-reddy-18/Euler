3f6569ad08421c9f68271000f1817ab061d8f9c1
ravindrareddyy88 <ravindrareddyy88@gmail.com>
2025-04-29 19:16:54 -0500

    O(n^2) with two loops only and code using more of the constraints a<b<c and a+b+c=1000

--- src/main/java/org/example/Problem9/Problem9.java
+++ src/main/java/org/example/Problem9/Problem9.java
@@ -1,14 +1,14 @@
 package org.example.Problem9;
 
 public class Problem9 {
-    static boolean tri(int a,int b,int c){return Math.pow(a,2)+Math.pow(b,2)==Math.pow(c,2);}
+    static boolean tri(int a,int b,int c){
+        return a*a+b*b==c*c;}
     public static void main(String[] arfs){
-        for(int c=997;c>0;c--){
-         for(int b=2;b<c;b++){
-        for(int a=1;a<b;a++){
-            if(a+b+c==1000&&tri(a,b,c)){System.out.println(a*b*c);return;}
-                }
-            }
-        }
+       for(int a=1;a<=332;a++){
+           for(int b=a+1;b<(1000-a)/2;b++){
+               int c=1000-a-b;
+               if(tri(a,b,c)){System.out.println(a*b*c);return;}
+           }
+       }
     }
 }
